539e52d638391075709245c3983038f0bcb5e05c
Alexandre Delapierre <alexandredelapierre@gmail.com>
2022-06-28 20:12:01 +0200

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,13 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+   },
+    'my pizza': {
+        'ingredients': {'mozzarella': 3, 'garlic': 5},
+        'price': 11,
+    },
+    'Calabraise': {
+        'ingredients': {'olive': 3,'gorgonzola': 4, 'pepper': 5, 'onion': 4, 'ham': 3},
+        'price': 13,
     }
 }
